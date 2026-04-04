3570d3d7663478d444cb388b9e3a8fafdef0a0ec
Sowparna Shome <soupornoshome@gmail.com>
2026-04-04 15:21:38 +0530

    chore: console.log remove

--- src/app.ts
+++ src/app.ts
@@ -5,21 +5,12 @@ import logger from './utils/logger';
 const PORT = env.PORT;
 
 const server = app.listen(PORT, () => {
-  logger.info(`\n💳 Payments & Subscriptions API`);
+  logger.info(`💳 Payments & Subscriptions API`);
   logger.info(`🚀 Server running on port ${PORT}`);
   logger.info(`🌍 Environment  : ${env.NODE_ENV}`);
   logger.info(`🩺 Health check : http://localhost:${PORT}/healthz`);
   logger.info(`🚀 Ready check : http://localhost:${PORT}/readyz`);
   logger.info(`📡 API prefix   : http://localhost:${PORT}/api/v1`);
-  //
-  console.log(`\n💳 Payments & Subscriptions API`);
-  console.log(`━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━`);
-  console.log(`🚀 Server running on port ${PORT}`);
-  console.log(`🌍 Environment  : ${env.NODE_ENV}`);
-  console.log(`🩺 Health check : http://localhost:${PORT}/healthz`);
-  console.log(`🚀 Ready check : http://localhost:${PORT}/readyz`);
-  console.log(`📡 API prefix   : http://localhost:${PORT}/api/v1`);
-  console.log(`━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n`);
 });
 
 //  Graceful shutdown
